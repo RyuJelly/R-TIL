f407dd42a8bfd22159aaaacba12c3e2f8ad798c0
Jeryong Ryu <wima90@gmail.com>
2020-09-17 22:48:37 +0900

    API crawling

---
+++ openapi.R
@@ -0,0 +1,44 @@
+library(httr)
+library(rvest)
+library(XML)
+library(rtweet) 
+
+# OPEN API 실습1
+rm(list=ls())
+searchUrl<- "https://openapi.naver.com/v1/search/blog.xml"
+Client_ID <- "izGsqP2exeThwwEUVU3x"
+Client_Secret <- "WrwbQ1l6ZI"
+
+query <- URLencode(iconv("맛집","euc-kr","UTF-8"))
+url <- paste0(searchUrl, "?query=", query, "&display=100")
+doc <- GET(url, add_headers("Content_Type" = "application/xml",
+                            "X-Naver-client-Id" = Client_ID, "X-naver-Client-Secret" = Client_Secret))
+doc2 <- htmlParse(doc, encoding="UTF-8")
+text<- xpathSApply(doc2, "//item/description", xmlValue)
+text<- gsub("(</?b>)", "", text)
+text<- gsub("&.+t", "", text)
+write.table(text, "naverblog.txt")
+
+# OPEN API 실습2
+appname <- "edu_data_collection"
+api_key <- "RvnZeIl8ra88reu8fm23m0bST"
+api_secret <- "wTRylK94GK2KmhZUnqXonDaIszwAsS6VPvpSsIo6EX5GQLtzQo"
+access_token <- "959614462004117506-dkWyZaO8Bz3ZXh73rspWfc1sQz0EnDU"
+access_token_secret <- "rxDWfg7uz1yXMTDwijz0x90yWhDAnmOM15R6IgC8kmtTe"
+twitter_token <- create_token(
+  app = appname,
+  consumer_key = api_key,
+  consumer_secret = api_secret,
+  access_token = access_token,
+  access_secret = access_token_secret)
+
+key <- "코로나"
+key <- enc2utf8(key)
+result <- search_tweets(key, n=100, token = twitter_token)
+str(result)
+result$retweet_text
+content <- result$retweet_text
+content <- gsub("[[:lower:][:upper:][:punct:][:cntrl:]]", "", content)
+write.table(content, "twitter.txt")
+
+# OPEN API 실습3
